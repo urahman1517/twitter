e4c6444edf2251153adb685a60b5a9fd13071465
urahman1517 <75455200+urahman1517@users.noreply.github.com>
2021-08-18 02:17:09 -0700

    Update settings.py

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -132,24 +132,22 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
+# https://docs.djangoproject.com/en/3.1/howto/static-files
 cloudinary.config(
   cloud_name = "djtmoizx5",
   api_key = "991559164973569",
   api_secret = "ZYUTj6w4yboXTUqYX9BKyP5scgI"
   )
 
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS =[
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
+    
 
 
 #Default primary key field type
 #https://docs.djangoproject.com/en/3.2/ref/setting/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
-
